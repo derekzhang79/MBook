794bb710904ed37d35cc5cc6bbec53792defc05e
Tom Callon <tomcallon2010@gmail.com>
2012-07-13 23:58:04 +0800

    update the uitabbarcontroller  catalogery .

---
+++ MBook/UITabBarController+Rotation.m
@@ -0,0 +1,13 @@
+//
+//  UITabBarController+Rotation.m
+//  MBook
+//
+//  Created by Tom Callon (Hotmail) on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UITabBarController+Rotation.h"
+
+@implementation UITabBarController (Rotation)
+
+@end
